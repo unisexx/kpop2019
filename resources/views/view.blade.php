35f761d24e76794c9d8d975d0fd9789e11dfece9
unisexx <unisexx@gmail.com>
2019-04-08 21:52:54 +0700

    updated

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -2,18 +2,13 @@
 
 <div class="fh5co-narrow-content">
     <div class="row">
-
-        <div class="col-md-12 animate-box" data-animate-effect="fadeInLeft">
-            <figure class="text-center">
-                <img src="{{ $rs->image }}" alt="{{ $rs->title }}" class="img-responsive">
-            </figure>
-        </div>
         
         <div class="col-md-12 col-md-offset-2 animate-box" data-animate-effect="fadeInLeft">
             
             {!! $rs->detail !!}
 
         </div>
+
     </div>
 
 </div>
