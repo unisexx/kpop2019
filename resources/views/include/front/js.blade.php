421d285ad5671d3d49237a5dc9b2aee5536e77f8
unisexx <unisexx@gmail.com>
2019-04-12 16:42:02 +0700

    updated

--- resources/views/include/front/js.blade.php
+++ resources/views/include/front/js.blade.php
@@ -24,3 +24,46 @@ $('ducument').ready(function(){
 
 });
 </script>
+
+
+<button onclick="topFunction()" id="toTopBtn" title="Go to top">Top</button>
+<style>
+#toTopBtn {
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 99;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  background-color: #27cacc;
+  color: white;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 4px;
+  transition: background-color 0.5s ease;
+}
+
+#toTopBtn:hover {
+  background-color: #17a2b8;
+}
+</style>
+<script>
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("toTopBtn").style.display = "block";
+  } else {
+    document.getElementById("toTopBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
